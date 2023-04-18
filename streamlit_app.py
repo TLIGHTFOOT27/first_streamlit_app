226020d85133d7b7a5eef0aa2f2afcfe1bb7f162
TLIGHTFOOT27 <130756237+TLIGHTFOOT27@users.noreply.github.com>
2023-04-18 16:48:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,6 @@ streamlit.write=('The user entered', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-streamlit.text(fruityvice_response.json())
 
 #The below line of code flattens the json fruityvice_response data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
