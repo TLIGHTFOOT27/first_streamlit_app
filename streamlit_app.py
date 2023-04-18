f181e8eedbf7bbbc648c77a9a6b3d2196fe2c3a3
TLIGHTFOOT27 <130756237+TLIGHTFOOT27@users.noreply.github.com>
2023-04-18 16:06:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Moms New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣  Omega 3 & Blueberry Oatmeal')
@@ -25,8 +25,8 @@ streamlit.dataframe(fruits_to_show )
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
 
 #The below line of code flattens the json fruityvice_response data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
