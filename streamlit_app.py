b0bf8ca38a74e0420b077117f938fa8631a58798
TLIGHTFOOT27 <130756237+TLIGHTFOOT27@users.noreply.github.com>
2023-04-19 16:08:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,19 +25,20 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show )
 
+
 #New Section to display fruitvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice=streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write=('The user entered', fruit_choice)
+try:
+  fruit_choice=streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+     streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-#The below line of code flattens the json fruityvice_response data
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#outputs fruityvice_normalized data code above to the streamlit output window.
-streamlit.dataframe(fruityvice_normalized)
-
+Except URLError as e:
+    streamlit.error()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
