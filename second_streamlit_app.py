8d407182cf479d6b338d7a236219d77d3615b620
TLIGHTFOOT27 <130756237+TLIGHTFOOT27@users.noreply.github.com>
2023-04-26 11:44:02 +0100

    Update second_streamlit_app.py

--- second_streamlit_app.py
+++ second_streamlit_app.py
@@ -5,15 +5,19 @@ import pandas
 streamlit.title("Zena's Amazing Athleisure Catalog")
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-with my_cnx.cursor() as my_cur:
-         my_sweatshirt_list=my_cur.execute("select $1 as style from ZENAS_ATHLEISURE_DB.PRODUCTS.SWEATSUITS")   
-         my_cnx.close()
-         streamlit.dataframe(my_sweatshirt_list)        
+my_cur = my_cnx.cursor()
 
-my_sweatshirt_list = my_sweatshirt_list.set_index('0')
+my_cur.execute("select color_or_style from ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG_FOR_WEBSITE")   
+my_catalog = my_cur.fetchall()
+
+
+
+#streamlit.dataframe(my_sweatshirt_list)        
+ 
+#my_sweatshirt_list = my_sweatshirt_list.set_index('0')
          
-fruits_selected=streamlit.multiselect("Pick a sweatsuit color or style:", list(my_sweatshirt_list))
-streamlit.dataframe(fruits_selected)
+#fruits_selected=streamlit.multiselect("Pick a sweatsuit color or style:", list(my_sweatshirt_list))
+#streamlit.dataframe(fruits_selected)
 
     
 
