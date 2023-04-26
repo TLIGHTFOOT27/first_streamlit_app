977e4fb3cfd1819fc0f39b3400aece66f1d469b1
TLIGHTFOOT27 <130756237+TLIGHTFOOT27@users.noreply.github.com>
2023-04-26 12:03:23 +0100

    Update second_streamlit_app.py

--- second_streamlit_app.py
+++ second_streamlit_app.py
@@ -1,20 +1,26 @@
+# Add the import steps to bring in the different packages
 import streamlit
 import snowflake.connector
 import pandas
 
+# Give the page a title
 streamlit.title("Zena's Amazing Athleisure Catalog")
 
+# Connect to Snowflake then assign the cursor
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
+# Execute a SQL step to pull back data from snowflake and save it into a variable 'my_catalog'
 my_cur.execute("select color_or_style from ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG_FOR_WEBSITE")   
 my_catalog = my_cur.fetchall()
 
+# put the dafta into a pandas dataframe
 df=pandas.DataFrame(my_catalog)
-#streamlit.write(df)
 
+# create a list from the above dataframe, with '0' being the positional variable chosen
 color_list=df[0].tolist()
 
+# Create a selection box based on the list above
 option=streamlit.selectbox("Pick a sweatsuit color or style:", list(color_list))
 
 # We'll build the image caption now, since we can
